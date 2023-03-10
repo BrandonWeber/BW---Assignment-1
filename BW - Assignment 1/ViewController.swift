352be2ea4cc322d38e97210e49e6a7a970d6c09e
Brandon Weber <bweber425@gmail.com>
2023-03-10 17:21:58 -0500

    done

--- BW - Assignment 1/ViewController.swift
+++ BW - Assignment 1/ViewController.swift
@@ -12,8 +12,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        //Element 1 - Welcome message
+                print("Hi! my name is Brandon and my two favorite numbers are 19 and 27")
+                let name = "Brandon"
+                var num1 = 19
+                var num2 = 27
+        //Element 2 - which is bigger
+                
+                if num1 < num2 {
+                    print("My second favorite number is smaller than my first.")
+                }
+        //Element 3 - if numbers are positive or negative = if else
+                var sum  = num1 + num2
+                
+                if sum > 0 {
+                    print("My numbers added together are positive.")
+                } else {
+                    print("My numbers added together are negative.")
+                }
+        //Element 5 - while statement - if num1 is smaller. num2 is bigger
+                while num1 < num2 {
+                    print("My first favorite number will always be smaller than my second.")
+                }
+        //Element 4 - array
+                                var fruits = ["strawberries", "grapes", "watermelon"]
+                        print("My three favorite fruits are \(fruits)")
+            }
+        }
 
 
-}
+
+
 
